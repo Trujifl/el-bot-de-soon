f3e6674fd9132bd4a187993dc52245fdc111fd14
Trujifl <trujicrypto@gmail.com>
2025-07-23 23:43:54 -0400

    Update filters.py

--- src/utils/filters.py
+++ src/utils/filters.py
@@ -1,25 +1,15 @@
-from telegram import Message
-from telegram.ext import MessageFilter
-from src.config import TOPIC_ID
+from telegram import Update
+from telegram.ext import BaseFilter
 
-
-class MentionedBotFilter(MessageFilter):
-    def filter(self, message: Message) -> bool:
-        return (
-            message is not None
-            and message.entities is not None
-            and any(
-                entity.type == "mention"
-                and message.parse_entity(entity).lower().startswith("@")
-                for entity in message.entities
-            )
+class MentionedBotFilter(BaseFilter):
+    def filter(self, message: Update.message) -> bool:
+        if not message.entities or not message.text or not message.bot:
+            return False
+        return any(
+            e.type == "mention" and f"@{message.bot.username}" in message.text
+            for e in message.entities
         )
 
-
-class TopicFilter(MessageFilter):
-    def filter(self, message: Message) -> bool:
-        return (
-            message is not None
-            and message.is_topic_message
-            and message.message_thread_id == TOPIC_ID
-        )
+class TopicFilter(BaseFilter):
+    def filter(self, message: Update.message) -> bool:
+        return message.message_thread_id == 8183 
