06904e356a94c857433b36415231648825ee4763
Trujifl <trujicrypto@gmail.com>
2025-07-25 14:49:57 -0400

    Update render_main.py
    actualizacion a modo priv

--- render_main.py
+++ render_main.py
@@ -15,7 +15,7 @@ from src.config import (
     logger,
     BotMeta
 )
-from src.handlers.base import setup_base_handlers
+from src.handlers.base import setup_base_handlers, handle_comando_general
 from src.handlers.crypto import precio_cripto
 from src.handlers.post import PostHandler
 from src.handlers.resumen import ResumeHandler
@@ -40,11 +40,14 @@ async def set_commands():
 
 def setup_handlers():
     setup_base_handlers(application)
+
     application.add_handler(CommandHandler("precio", precio_cripto))
     application.add_handler(CommandHandler("post", post_handler.handle))
     application.add_handler(CommandHandler("resumen_texto", resume_handler.handle_resumen_texto))
     application.add_handler(CommandHandler("resumen_url", resume_handler.handle_resumen_url))
     application.add_handler(CallbackQueryHandler(post_handler.handle_confirmation, pattern="^(confirm|cancel)_post_"))
+
+    application.add_handler(MessageHandler(filters.COMMAND, handle_comando_general))
 
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_consulta_token))
 
@@ -73,7 +76,6 @@ def health_check():
 
 if __name__ == '__main__':
     setup_handlers()
-
     application.run_webhook(
         listen="0.0.0.0",
         port=int(os.getenv("PORT", 8080)),
