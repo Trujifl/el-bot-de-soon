214a2e5d4e0731cce8dac47f794aa4f8579047ea
Trujifl <trujicrypto@gmail.com>
2025-07-26 00:09:47 -0400

    Update filters.py

--- src/utils/filters.py
+++ src/utils/filters.py
@@ -1,7 +1,6 @@
-from telegram import Message
-from telegram.ext.filters import MessageFilter
+from telegram import Message, Update
+from telegram.ext import MessageFilter
 from src.config import GROUP_ID, TOPIC_ID
-
 
 class MentionedBotFilter(MessageFilter):
     def filter(self, message: Message) -> bool:
@@ -13,7 +12,13 @@ class MentionedBotFilter(MessageFilter):
             for entity in message.entities
         )
 
+    def __call__(self, update: Update) -> bool:
+        return update.message is not None and self.filter(update.message)
+
 
 class TopicFilter(MessageFilter):
     def filter(self, message: Message) -> bool:
         return message.chat.id == GROUP_ID and message.message_thread_id == TOPIC_ID
+
+    def __call__(self, update: Update) -> bool:
+        return update.message is not None and self.filter(update.message)
